f745bd60cf71bf113410e7b83c08659e0dc5cbdd
vaalentine2 <linyuanmei23@gmail.com>
2026-06-29 18:26:27 +0900

    Change hidden data in suspicious mode

--- make_demo_image.py
+++ make_demo_image.py
@@ -14,7 +14,7 @@ png_data = base64.b64decode(
 )
 
 if mode == "suspicious":
-    png_data += b"\nCONFIDENTIAL: This is harmless demo hidden data for the StegoGuard lab.\n"
+    png_data += b"\nPRIVATEKEY: lolol this is secret.\n"
 
 Path("images/student_image.png").write_bytes(png_data)
 
